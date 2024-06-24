294533c62cfbd941c10f941dc575d41623b1c761
JFKongphop <93483308+JFKongphop@users.noreply.github.com>
2024-06-24 23:30:50 +0700

    add completed thailand dropdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,17 @@
+import ThailandDropdown from "@/components/ThailandDropdown/ThailandDropdown";
+import { useState } from "react";
+
 const index = () => {
+  const [fixedScreen, setFixScreen] = useState<boolean>(false);
+
+  const fixedScreenHandler = (status: boolean) => {
+    setFixScreen(status);
+  };
+  
   return (
-    <div className="text-red-500">index</div>
+    <div className="">
+      <ThailandDropdown onFixedScreen={fixedScreenHandler} />
+    </div>
   )
 }
 
